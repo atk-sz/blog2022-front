7c1dc46ddf60552789f789524fddb676d876bb29
Syed Zaid <syed.zaid525@gmail.com>
2022-02-02 20:38:04 +0530

    about page template ready

--- src/views/About.js
+++ src/views/About.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Header } from "../components";
 import "./css/About.css";
+// import introPic from "../assests/NiceRoadNoBG.jpg";
+// import introPic from "../assests/MyPic.png";
 
 const About = () => {
   return (
     <div>
       <Header />
       <section className="about">
-        <h1>About</h1>
+        {/* <div className="pic">
+          <img src={introPic} alt="" />
+        </div>
+        <div className="intro"></div> */}
       </section>
     </div>
   );
