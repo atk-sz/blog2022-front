aae52d1dc2d526585017e70ef396cbf2589e3e88
Syed Zaid <syed.zaid525@gmail.com>
2022-05-12 19:06:54 +0530

    converting to single page application only main page done

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/Main.css";
+
+const Main = () => {
+  return (
+    <section className="main" id="main">
+      <div className="content">
+        <div className="name-tag">
+          <h1 className="name">SYED ZAID</h1>
+        </div>
+        <div className="bio-tag">
+          <h5 className="bio">Engineer / Counsler / Poet</h5>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Header } from "../components";
-import "./css/Home.css";
+import { Header, Main } from "../components";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <section className="home" id="home">
-        <div className="content">
-          <div className="name-tag">
-            <h1 className="name">SYED ZAID</h1>
-          </div>
-          <div className="bio-tag">
-            <h5 className="bio">Engineer / Counsler / Poet</h5>
-          </div>
-        </div>
-      </section>
+      <Main />
     </div>
   );
 };
