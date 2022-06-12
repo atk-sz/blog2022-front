2a666541f6299cc38385d7e14d0c841ee3e41cd9
Syed Zaid <syed.zaid525@gmail.com>
2022-06-12 14:52:26 +0530

    added about svg

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./css/About.css";
 import { Header } from "../components";
+// import ReactLogo from './logo.svg';
+import AboutLogo from "../assests/svgs/about.svg";
 
 const About = () => {
   return (
     <div id="about">
       <Header />
+      <img src={AboutLogo} alt="About Logo" />
       <h1>Aboz</h1>
     </div>
   );
