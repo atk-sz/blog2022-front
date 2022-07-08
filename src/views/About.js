0dae2fc4ad3321dc968fbf8a599239ffb3063d31
Syed Zaid <syed.zaid525@gmail.com>
2022-07-08 08:10:20 +0530

    added age calc

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/About.css";
 import { Header } from "../components";
 import "../assests/svgs/about.svg";
@@ -6,6 +6,31 @@ import AboutLogo from "../assests/svgs/about.svg";
 // import AboutSVG from "../assests/svgs/AboutSVG";
 
 const About = () => {
+  let [age, setAge] = useState(0);
+
+  useEffect(() => {
+    ageCal();
+  }, []);
+
+  const ageCal = () => {
+    var dob = new Date("10/22/1998");
+    //calculate month difference from current date in time
+    // var month_diff = Date.now("10/22/2024") - dob.getTime();
+    var month_diff = new Date("10/21/2022") - dob.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+
+    //display the calculated age
+    setAge(age);
+  };
+
   return (
     <div id="about">
       <Header />
@@ -13,8 +38,8 @@ const About = () => {
         <div className="about-content">
           {/* Hello to all my readers, iam a 23 year old software engineer from Bangalore.  */}
           <p>
-            Hello to all my readers, I am from Bangalore, 23 years old, Software
-            Engineer, Poet, Social Worker, Counselor and a{" "}
+            Hello to all my readers, I am from Bangalore, {age} years old,
+            Software Engineer, Poet, Social Worker, Counselor and a{" "}
             <em className="friends-em">
               F<span style={{ color: "red" }}>.</span>R
               <span style={{ color: "blue" }}>.</span>I
@@ -28,8 +53,8 @@ const About = () => {
           <p>
             Fooo!! So, basically iam a developer by passion, i write poetry when
             iam gloomy, iam a part time social worker and i like to help people
-            with thier psychological issues and last but not the least iam a crazy
-            ass fan of The Show FRIENDS
+            with thier psychological issues and last but not the least iam a
+            crazy ass fan of The Show FRIENDS
           </p>
         </div>
         <div className="about-img-div">
