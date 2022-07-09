dae23e01a6fc4550494ad79b40ef6785ce4b879f
Syed Zaid <syed.zaid525@gmail.com>
2022-07-09 22:41:45 +0530

    backend update check

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const sendMail = async (values) => {
+  console.log("lst");
+  console.log(process.env.REACT_APP_BACKEND_API);
   return await axios.post(`${process.env.REACT_APP_BACKEND_API}/sendmail`, {
     values,
   });
