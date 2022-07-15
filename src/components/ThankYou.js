b5384fac339168217ecd8d80a63ae199b15f2cf1
Syed Zaid <syed.zaid525@gmail.com>
2022-07-15 14:55:30 +0530

    thank responsive, evrything is responsive for now

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -23,26 +23,20 @@ const ThankYou = () => {
 
   return (
     <div className="payment-success">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mx-auto mt-5">
-            <div className="payment">
-              <div className="payment_header">
-                <div className="check">
-                  <i className="fa fa-check" aria-hidden="true">
-                    {" "}
-                    <FaCheckCircle color="green" />
-                  </i>
-                </div>
-              </div>
-              <div className="content">
-                <h1>Payment Success !</h1>
-                <p>Thank you for your support😊. </p>
-              </div>
-              <h6>You'll be redirected to home within {secs} secs.</h6>
-            </div>
+      <div className="payment">
+        <div className="payment_header">
+          <div className="check">
+            <i className="fa fa-check" aria-hidden="true">
+              {" "}
+              <FaCheckCircle color="green" />
+            </i>
           </div>
         </div>
+        <div className="content">
+          <h1>Payment Success !</h1>
+          <p>Thank you for your support😊. </p>
+        </div>
+        <h6>You'll be redirected to home within {secs} secs.</h6>
       </div>
     </div>
   );
